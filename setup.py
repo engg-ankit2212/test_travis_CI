5c7a9bb6d673b1fe9f9d5e9dad36e48d8ac1fe2f
celesankit2212 <ankit.m@celestialsys.com>
2020-09-30 11:39:44 +0530

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='test_travis_CI',
+    version='',
+    packages=[
+        'config'
+    ] + find_packages(),
+    url='',
+    license='',
+    author='celesankit2212',
+    author_email='',
+    description=''
+)
